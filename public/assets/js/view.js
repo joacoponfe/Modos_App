9b453472d47df6873708cd05a6d85d7cc15af705
Joaquín Ponferrada <joacoponfe@gmail.com>
2023-03-21 18:41:41 -0300

    ajustes

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -35,6 +35,7 @@ var tagCloud = TagCloud('.content', myTags,{
 
 //To change the color of text randomly
 //var colors = ['#34A853', '#FBBC05', '#4285F4', '#7FBC00', 'FFBA01', '01A6F0', '#F00105', '#AB0C76'];
-var colors = ['#044d59', '#59044f', '#ab0322', '#03ab87', '#abe305', '#49574a', '#d0e60e']
+//var colors = ['#044d59', '#59044f', '#ab0322', '#03ab87', '#abe305', '#49574a', '#d0e60e']
+var colors = ['#044d59'];
 var random_color = colors[Math.floor(Math.random() * colors.length)];
 document.querySelector('.content').style.color = random_color;
