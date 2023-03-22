ba9a8de0cec30d261e5638700095011000b13511
Joaquín Ponferrada <81579256+joacoponfe@users.noreply.github.com>
2023-03-22 12:07:22 -0300

    Algunas páginas de totems

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -5,11 +5,11 @@ const saveFile =(e) => {
     const age = document.getElementById('age');
     const sex = document.getElementById('sex');
     const education = document.getElementById('education');
-    const education_num = document.getElementById('education_num');
+    //const education_num = document.getElementById('education_num');
     const musical_knowledge = document.getElementById('musical-knowledge');
     const knows_mode = document.getElementById('knows-mode');
 
-    const formData = {"age":age.value, "sex":sex.value, "education":education.value, "education_num":education_num.value, 
+    const formData = {"age":age.value, "sex":sex.value, "education":education.value, 
     "musical_knowledge":musical_knowledge.value, "knows_mode":knows_mode.value};
 
     console.log(formData)
