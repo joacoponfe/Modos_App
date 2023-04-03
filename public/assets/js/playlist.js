aaa7caef0efa7b987171b33024de49ce847d3570
Joaquín Ponferrada <joacoponfe@gmail.com>
2023-04-03 13:02:04 -0300

    Página totems y playlist demo

---
+++ public/assets/js/playlist.js
@@ -0,0 +1,96 @@
+const player = document.getElementById("player");
+const audio = document.getElementById("audio");
+const playPause = document.getElementById("play-pause");
+const prev = document.getElementById("prev");
+const next = document.getElementById("next");
+const playlist = document.getElementById("songs");
+const progressBar = document.getElementById("progress-bar");
+const progress = document.querySelector(".progress");
+const volume = document.getElementById("volume");
+const currentTime = document.querySelector("#current-time");
+const duration = document.querySelector("#duration");
+
+let currentSong = 0;
+
+playlist.addEventListener("click", (e) => {
+  if (e.target.tagName === "LI") {
+    currentSong = Array.from(playlist.children).indexOf(e.target);
+    audio.src = e.target.getAttribute("data-src");
+    audio.play();
+    setActiveSong(currentSong);
+  }
+});
+
+playPause.addEventListener("click", () => {
+  if (audio.paused) {
+    audio.play();
+    playPause.innerHTML = '<i class="bi bi-pause-fill"></i>';
+  } else {
+    audio.pause();
+    playPause.innerHTML = '<i class="bi bi-play-fill"></i>';
+  }
+});
+
+prev.addEventListener("click", () => {
+  currentSong--;
+  if (currentSong < 0) {
+    currentSong = playlist.children.length - 1;
+  }
+  audio.src = playlist.children[currentSong].getAttribute("data-src");
+  audio.play();
+  setActiveSong(currentSong);
+});
+
+next.addEventListener("click", () => {
+  currentSong++;
+  if (currentSong === playlist.children.length) {
+    currentSong = 0;
+  }
+  audio.src = playlist.children[currentSong].getAttribute("data-src");
+  audio.play();
+  setActiveSong(currentSong);
+});
+
+volume.addEventListener("input", (e) => {
+  audio.volume = e.target.value;
+});
+
+audio.addEventListener("timeupdate", (e) => {
+  const percent = (audio.currentTime / audio.duration) * 100;
+  progress.style.width = `${percent}%`;
+  currentTime.textContent = formatTime(audio.currentTime);
+  duration.textContent = formatTime(audio.duration);
+});
+
+function formatTime(time) {
+  const minutes = Math.floor(time / 60);
+  const seconds = Math.floor(time % 60);
+  return `${minutes}:${seconds.toString().padStart(2, "0")}`;
+}
+
+progressBar.addEventListener("click", (e) => {
+  const percent =
+    (e.clientX - progressBar.offsetLeft) / progressBar.offsetWidth;
+  audio.currentTime = percent * audio.duration;
+  progress.style.width = `${percent}%`;
+});
+
+audio.addEventListener("ended", (e) => {
+  if (currentSong === playlist.children.length - 1) {
+    currentSong = 0;
+  } else {
+    currentSong++;
+  }
+  audio.src = playlist.children[currentSong].getAttribute("data-src");
+  audio.play();
+  setActiveSong(currentSong);
+});
+
+const setActiveSong = (index) => {
+  // remove "active" class from previously active song
+  playlist.querySelector(".active-song").classList.remove("active-song");
+
+  // add "active" class to currently playing song
+  const activeSong = playlist.querySelectorAll("li")[index];
+  activeSong.classList.add("active-song");
+};
---
+++ public/assets/js/totems_main.js
@@ -0,0 +1,103 @@
+// Create an audio element
+const audioPlayer = document.createElement('audio');
+// Set the source of the audio file
+audioPlayer.src = 'music/J1.mp3';
+// Set the controls attribute to display the audio player controls
+audioPlayer.controls = true;
+
+// Create Word Cloud element
+const wordCloud = document.createElement("span");
+wordCloud.classList.add('tagcloud');
+const tagContainer = '.tagcloud';
+
+const words = [
+    'alegría', 'esperanza', 'contento', 'paz',
+    'bueno', 'soleado', 'armonioso', 'dulce',
+    'aire', 'aire', 'alegría', 'paz',
+    'aire', 'paz', 'paz', 'yendo',
+    'árboles', 'vida', 'cajas', 'violeta',
+    'castillo', 'monitores', 'papeles', 'impresora',
+    'auriculares', 'billetera', 'magnífico', 'teclas',
+    'piano', 'treinta', 'treinta', 'dieciocho',
+    'televisor', 'computadora', 'escritorio', 'escritorio',
+    'mate', 'silla', 'silla', 'billetera', 'parlante', 'parlante', 'dios', 'guerra',
+    'guerra', 'silla', 'termo'
+];
+
+const options = {radius:200, maxSpeed:'fast', initSpeed:'fast', direction:135, keep:true};
+//const tagCloud = TagCloud(tagContainer, words, options);
+
+// Create emotional thermometer element
+
+
+// Create image element
+const image = document.createElement('img');
+image.src = "https://i.pinimg.com/originals/3c/f8/41/3cf8412096f10f0847e6e689fde63775.jpg";
+
+// Create playlist element
+const player = document.createElement("div");
+const audio = document.createElement("audio");
+const controls = document.createElement("div");
+const progressBar = document.createElement("div");
+
+
+// Get references to the buttons in the sidebar menu
+const buttons = document.querySelectorAll('.sidebar a');
+
+// Add a counter for word cloud button clicks (to avoid rendering more than once)
+var wordCloudButtonClicks = 0;
+//console.log(wordCloudButtonClicks);
+
+// Add a click event listener to each button
+buttons.forEach(button => {
+    button.addEventListener('click', event => {
+        // Get button ID
+        //const buttonID = event.target.getAttribute('data-content');
+
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Set all buttons to "inactive"
+        buttons.forEach(button => {
+            button.setAttribute('class', '');
+        });
+        
+        // Get the value of the "data-content" attribute of the clicked button
+        const content = button.getAttribute('data-content');
+
+        // Increment the click counter for the word cloud button
+        if (content === 'wordcloud') {
+            wordCloudButtonClicks++;
+        }
+        //console.log(wordCloudButtonClicks);
+
+        // Update the content in the "container" div based on the value of the "data-content" attribute
+        if (content === 'music') {
+            document.getElementById('container').innerHTML = "<h2>Melodía</h2><br>Volvé a escuchar la melodía<br>";
+            document.getElementById('container').appendChild(audioPlayer);
+            button.setAttribute('class', "active");
+        } else if (content === 'wordcloud') {
+            document.getElementById('container').innerHTML = '<h2>Conceptos predominantes</h2><br>';
+            document.getElementById('container').appendChild(wordCloud);
+            console.log(container);
+            if (wordCloudButtonClicks === 1) {
+                var tagCloud = TagCloud(tagContainer, words, options);
+            };
+            button.setAttribute('class', "active");
+        } else if (content === 'thermometer') {
+            document.getElementById('container').innerHTML = 'Themometer content goes here';
+            button.setAttribute('class', "active");
+        } else if (content === 'images') {
+            document.getElementById('container').innerHTML = 'Imagen generada con inteligencia artificial a partir de tu texto <br>';
+            document.getElementById('container').appendChild(image);
+            button.setAttribute('class', "active");
+        } else if (content === 'you-and-others') {
+            document.getElementById('container').innerHTML = 'You and others content goes here';
+            button.setAttribute('class', "active");
+        } else if (content === 'songs') {
+            document.getElementById('container').innerHTML = '<h2>Canciones representativas</h2>';
+            button.setAttribute('class', "active");
+        }
+    });
+});
+
