3d1875a681250cf9843f558f5b641465d81d2474
joacoponfe <joacoponfe@gmail.com>
2023-05-04 19:06:04 -0300

    Avances en totems_main.js

--- public/assets/js/thermometer.js
+++ public/assets/js/thermometer.js
@@ -1,31 +1,29 @@
-const alegria = document.getElementById("alegria-value");
-const tristeza = document.getElementById("tristeza-value");
-const sorpresa = document.getElementById("sorpresa-value");
-const asco = document.getElementById("asco-value");
-const miedo = document.getElementById("miedo-value");
-const enojo = document.getElementById("enojo-value");
-const otro = document.getElementById("otro-value");
-const pos = document.getElementById("pos-value");
-const neu = document.getElementById("neu-value");
-const neg = document.getElementById("neg-value");
+const alegria_value = document.getElementById("alegria-value");
+const tristeza_value = document.getElementById("tristeza-value");
+const sorpresa_value = document.getElementById("sorpresa-value");
+const asco_value = document.getElementById("asco-value");
+const miedo_value = document.getElementById("miedo-value");
+const enojo_value = document.getElementById("enojo-value");
+const otro_value = document.getElementById("otro-value");
+const pos_value = document.getElementById("pos-value");
+const neu_value = document.getElementById("neu-value");
+const neg_value = document.getElementById("neg-value");
 
-function setValues(alegria_percentage, tristeza_percentage, sorpresa_percentage, asco_percentage, miedo_percentage, enojo_percentage, otro_percentage, pos_percentage, neu_percentage, neg_percentage){
-    alegria.innerHTML = (alegria_percentage * 100).toString().concat("%");
-    tristeza.innerHTML = (tristeza_percentage * 100).toString().concat("%");
-    sorpresa.innerHTML = (sorpresa_percentage * 100).toString().concat("%");
-    asco.innerHTML = (asco_percentage * 100).toString().concat("%");
-    miedo.innerHTML = (miedo_percentage * 100).toString().concat("%");
-    enojo.innerHTML = (enojo_percentage * 100).toString().concat("%");
-    otro.innerHTML = (otro_percentage * 100).toString().concat("%");
-    pos.innerHTML = (pos_percentage * 100).toString().concat("%");
-    neu.innerHTML = (neu_percentage * 100).toString().concat("%");
-    neg.innerHTML = (neg_percentage * 100).toString().concat("%");
+export function setValues(alegria_percentage, tristeza_percentage, sorpresa_percentage, asco_percentage, miedo_percentage, enojo_percentage, otro_percentage, pos_percentage, neu_percentage, neg_percentage){
+    alegria_value.innerHTML = (alegria_percentage * 100).toString().concat("%");
+    tristeza_value.innerHTML = (tristeza_percentage * 100).toString().concat("%");
+    sorpresa_value.innerHTML = (sorpresa_percentage * 100).toString().concat("%");
+    asco_value.innerHTML = (asco_percentage * 100).toString().concat("%");
+    miedo_value.innerHTML = (miedo_percentage * 100).toString().concat("%");
+    enojo_value.innerHTML = (enojo_percentage * 100).toString().concat("%");
+    otro_value.innerHTML = (otro_percentage * 100).toString().concat("%");
+    pos_value.innerHTML = (pos_percentage * 100).toString().concat("%");
+    neu_value.innerHTML = (neu_percentage * 100).toString().concat("%");
+    neg_value.innerHTML = (neg_percentage * 100).toString().concat("%");
 
 };
 
-
-
-setValues(0.67, 0.05, 0.11, 0.04, 0.02, 0.04, 0.33, 0.25, 0.25, 0.5);
+//setValues(0.67, 0.05, 0.11, 0.04, 0.02, 0.04, 0.33, 0.25, 0.25, 0.5);
 
 
 
--- public/assets/js/totems_main.js
+++ public/assets/js/totems_main.js
@@ -1,14 +1,98 @@
 import { getCookie } from "./cookies.js";
 import { url } from "./config.js";
 
-// Get document elements
+// Get cookies
+const id_participant = getCookie('id_participant');
+
+// Get document elements, iframe and iframe document elements
 const mode_1 = document.getElementById("mode_1");
 const mode_2 = document.getElementById("mode_2");
 const id_string = document.getElementById("id_string");
 
-// Get cookies
-const id_participant = getCookie('id_participant');
-var current_mode = 1;
+function setThermometerValues(iframeDoc, alegria_percentage, tristeza_percentage, sorpresa_percentage, asco_percentage, miedo_percentage, enojo_percentage, otro_percentage, pos_percentage, neu_percentage, neg_percentage, decimal_places=1){
+    var alegria_value = iframeDoc.getElementById('alegria-value');
+    var tristeza_value = iframeDoc.getElementById("tristeza-value");
+    var sorpresa_value = iframeDoc.getElementById("sorpresa-value");
+    var asco_value = iframeDoc.getElementById("asco-value");
+    var miedo_value = iframeDoc.getElementById("miedo-value");
+    var enojo_value = iframeDoc.getElementById("enojo-value");
+    var otro_value = iframeDoc.getElementById("otro-value");
+    var pos_value = iframeDoc.getElementById("pos-value");
+    var neu_value = iframeDoc.getElementById("neu-value");
+    var neg_value = iframeDoc.getElementById("neg-value");
+
+    alegria_value.innerHTML = (alegria_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    tristeza_value.innerHTML = (tristeza_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    sorpresa_value.innerHTML = (sorpresa_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    asco_value.innerHTML = (asco_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    miedo_value.innerHTML = (miedo_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    enojo_value.innerHTML = (enojo_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    otro_value.innerHTML = (otro_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    pos_value.innerHTML = (pos_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    neu_value.innerHTML = (neu_percentage * 100).toFixed(decimal_places).toString().concat("%");
+    neg_value.innerHTML = (neg_percentage * 100).toFixed(decimal_places).toString().concat("%");
+
+    // Get the stylesheet containing the animation rules
+    var stylesheet = iframeDoc.styleSheets[1];
+    
+    // Find the animation rule we want to modify
+    const emotionRuleNames = ['alegria-bar', 'tristeza-bar', 'sorpresa-bar', 'asco-bar', 'miedo-bar', 'enojo-bar', 'otro-bar'];
+    const emotionPercentages = [alegria_percentage, tristeza_percentage, sorpresa_percentage, asco_percentage, miedo_percentage, enojo_percentage, otro_percentage];
+    for (var i = 0; i < emotionRuleNames.length; i++){
+        var emotionRuleName = emotionRuleNames[i];
+        var emotionPercentage = emotionPercentages[i];
+        var animationRule = null;
+        for (var j = 0; j < stylesheet.rules.length; j++) {
+            var rule = stylesheet.rules[j];
+            if (rule.type == CSSRule.KEYFRAMES_RULE && rule.name == emotionRuleName) {
+                animationRule = rule;
+                break; 
+            };
+        };
+
+        if (animationRule) {
+            // Modify the animation rule
+            var keyframes = animationRule.cssRules;
+            for (var k = 0; k < keyframes.length; k++){
+                var keyframe = keyframes[k];
+                if (keyframe.keyText == '100%'){
+                    var shadow_inset = (1-emotionPercentage)*100;
+                    keyframe.style.boxShadow = `inset -${shadow_inset}em 0 0 0 #f7f7f7`;
+                };
+            };
+        };
+
+    };
+
+    const sentimentRuleNames = ['positive-bar', 'neutral-bar', 'negative-bar'];
+    const sentimentPercentages = [pos_percentage, neu_percentage, neg_percentage];
+    for (var i = 0; i < sentimentRuleNames.length; i++){
+        var sentimentRuleName = sentimentRuleNames[i];
+        var sentimentPercentage = sentimentPercentages[i];
+        var animationRule = null;
+        for (var j = 0; j < stylesheet.rules.length; j++){
+            var rule = stylesheet.rules[j];
+            if (rule.type == CSSRule.KEYFRAMES_RULE && rule.name == sentimentRuleName) {
+                animationRule = rule;
+                break; 
+            };
+        };
+
+        if (animationRule) {
+            // Modify the animation rule
+            var keyframes = animationRule.cssRules;
+            for (var k = 0; k < keyframes.length; k++){
+                var keyframe = keyframes[k];
+                if (keyframe.keyText == '100%'){
+                    var width = sentimentPercentage*100;
+                    keyframe.style.width = `${width}%`;
+                };
+            };
+        };
+    };
+
+};
+
 
 // Pull info from database from user's last iteration (last two modes they listened to)
 const object = {};
@@ -93,7 +177,7 @@ var words = userData['word_lists'][0];
 var tagcloud;
 
 // Create emotional thermometer element
-
+var container_frame;
 
 // Create image element
 const image = document.createElement('img');
@@ -105,24 +189,21 @@ const audio = document.createElement("audio");
 const controls = document.createElement("div");
 const progressBar = document.createElement("div");
 
-
 // Get references to the buttons in the sidebar menus
-const buttons = document.querySelectorAll('.sidebar a');
+const sidebarButtons = document.querySelectorAll('.sidebar a');
 
 // Add a counter for word cloud button clicks (to avoid rendering more than once)
 var wordCloudButtonClicks = 0;
 
-// Add a click event listener to each button
-buttons.forEach(button => {
+// Add a click event listener to each button on the sidebar
+sidebarButtons.forEach(button => {
     button.addEventListener('click', event => {
-        // Get button ID
-        //const buttonID = event.target.getAttribute('data-content');
 
         // Prevent the default link behavior
         event.preventDefault();
 
         // Set all buttons to "inactive"
-        buttons.forEach(button => {
+        sidebarButtons.forEach(button => {
             button.setAttribute('class', '');
         });
         
@@ -142,15 +223,14 @@ buttons.forEach(button => {
         } else if (content === 'wordcloud') {
             document.getElementById('container').innerHTML = '<h2>Conceptos predominantes</h2><br>';
             document.getElementById('container').appendChild(wordCloud);
-            //if (wordCloudButtonClicks === 1) {
-            //    tagcloud = TagCloud(tagContainer, words, options);  // Creates word cloud element
-            //};
             if (typeof tagcloud === "undefined") { // If object does not already exist
                 tagcloud = TagCloud(tagContainer, words, options);  // Creates word cloud element 
              }
             button.setAttribute('class', "active");
         } else if (content === 'thermometer') {
-            document.getElementById('container').innerHTML = '<h2>Termómetro emocional</h2><iframe src="thermometer.html"  width="100%" height="1000px" style="border:none;"></iframe>';
+            document.getElementById('container').innerHTML = '<h2>Termómetro emocional</h2>'
+            document.getElementById('container').appendChild(container_frame);
+            container_frame.style.display = "block";
             button.setAttribute('class', "active");
         } else if (content === 'images') {
             document.getElementById('container').innerHTML = '<h2>Imagen</h2><br>';
@@ -161,40 +241,120 @@ buttons.forEach(button => {
             button.setAttribute('class', "active");
         } else if (content === 'songs') {
             document.getElementById('container').innerHTML = '<h2>Canciones representativas</h2><iframe src="playlist.html"  width="100%" height="1000px" style="border:none;"></iframe>';
+            document.getElementById('container').appendChild(container_frame);
+            container_frame.setAttribute("src", "playlist.html"); // Set iframe source as playlist.html
+            container_frame.removeAttribute("hidden"); // Make iframe visible 
             button.setAttribute('class', "active");
         }
     });
 });
 
+// Get references to the mode tabs in the sidebar menu
+const modeButtons = document.querySelectorAll('.sidebar button');
+
+// Add a click event listener to each mode button
+modeButtons.forEach(button => {
+    button.addEventListener('click', event => {
+
+        // Prevent the default link behavior
+        event.preventDefault();
+
+        // Set all buttons to "inactive"
+        modeButtons.forEach(button => {
+            button.setAttribute('class', '');
+        });
+
+        // Get the value of the "mode" attribute of the clicked button
+        const mode = button.getAttribute('id');
+        console.log(mode);
+        
+        // Update the content based on the selected mode
+        if (mode === 'mode_1') {
+            setData(0);
+            button.setAttribute('class', "active");
+        } else if (mode === 'mode_2') {
+            setData(1);
+            button.setAttribute('class', "active");
+        };
+    });
+});
+
 // Set data according to selected mode
-setData(0); // Defaults to first mode
+container_frame = setData(0); // Defaults to first mode
 
 function setData(id_mode){
-    var word_list = userData['word_lists'][id_mode];
-    var encoded_image = userData['encoded_images'][id_mode];
-    var embedding_path_folder = userData['embedding_path_folder'][id_mode];
-    var id_melody = userData['id_melody'][id_mode];
+    ///Updates information displayed on website according to the selected mode.
+    var word_list = userData['word_lists'][id_mode];                                                        // Get word list
+    var encoded_image = userData['encoded_images'][id_mode];                                                // Get encoded image
+    var embedding_path_folder = userData['embedding_path_folder'][id_mode];                                 // Get embeddings video
+    var id_melody = userData['id_melody'][id_mode];                                                         // Get id_melody
+    var alegria = userData['alegria'][id_mode];                                                             // Get "alegria"
+    var tristeza = userData['tristeza'][id_mode];                                                           // Get "tristeza"
+    var sorpresa = userData['sorpresa'][id_mode];                                                           // Get "sorpresa"
+    var asco = userData['asco'][id_mode];                                                                   // Get "asco"
+    var miedo = userData['miedo'][id_mode];                                                                 // Get "miedo"
+    var enojo = userData['enojo'][id_mode];                                                                 // Get "enojo"
+    var otro = userData['otro'][id_mode];                                                                   // Get "otro"
+    var pos = userData['pos'][id_mode];                                                                     // Get "pos"
+    var neu = userData['neu'][id_mode];                                                                     // Get "neu"
+    var neg = userData['neg'][id_mode];                                                                     // Get "neg"
     
-    audioPlayer.src = 'music/'.concat(id_melody,'.mp3') // Set melody
+    audioPlayer.src = 'music/'.concat(id_melody,'.mp3')                                                     // Set melody to be played
     
-    words = word_list  // Set word cloud
+    words = word_list                                                                                       // Set word list
     if (wordCloudButtonClicks >= 1) {
-        if (typeof tagcloud != "undefined") {   // If object exists
-            tagcloud.destroy();                 // destroy it
-         }
-        tagcloud = TagCloud(tagContainer, words, options);  // and create new one
+        if (typeof tagcloud != "undefined") {                                                               // If object exists
+            tagcloud.destroy();                                                                             // destroy it
+        }
+        tagcloud = TagCloud(tagContainer, words, options);                                                  // and create new one
     };
     
     // Set emotional thermometer
-    image.src = 'data:image/png;base64,'.concat(encoded_image);     // Set image source
+    if (typeof container_frame != "undefined") {                                                            // If thermometer exists
+        container_frame.remove();                                                                           // Destroy it
+        container_frame = document.createElement('iframe');                                                 // And create new one
+        container_frame.setAttribute("src", "thermometer.html?cache-buster=123");
+        container_frame.style.width = "100%";
+        container_frame.style.height = "1000px";
+        container_frame.style.border = "none";
+        container_frame.style.display = "block";
+        container_frame.addEventListener('load', function() {
+            var iframeDoc = container_frame.contentWindow.document;
+            setThermometerValues(iframeDoc, alegria, tristeza, sorpresa, asco, miedo, enojo, otro, pos, neu, neg);
+        });
+        
+        var activeButton = null;                                                                            // Only render thermometer if active sidebar button is "thermometer"
+        sidebarButtons.forEach(function(button) {
+            if (button.getAttribute("class") === "active") {
+              activeButton = button.getAttribute("id");
+            }
+        });
+          
+        if (activeButton) {
+            console.log('The active button is:', activeButton);
+          } else {
+            console.log('No button is currently active.');
+          }
+        if (activeButton === "thermometer"){
+            document.getElementById('container').appendChild(container_frame);
+        }
+        
+    } else {
+        container_frame = document.createElement('iframe');
+        container_frame.setAttribute("src", "thermometer.html?cache-buster=123");
+        container_frame.style.width = "100%";
+        container_frame.style.height = "1000px";
+        container_frame.style.border = "none";
+        container_frame.style.display = "none";
+        container_frame.addEventListener('load', function() {
+            var iframeDoc = container_frame.contentWindow.document;
+            setThermometerValues(iframeDoc, alegria, tristeza, sorpresa, asco, miedo, enojo, otro, pos, neu, neg);
+        });
+    };
+    
+
+    image.src = 'data:image/png;base64,'.concat(encoded_image);                                             // Set image source
     // Set embeddings
     // Set playlist
-
-}
-
-mode_1.onclick = function () {
-    setData(0);
-};
-mode_2.onclick = function () {
-    setData(1);
+    return container_frame
 };
