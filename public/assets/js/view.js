33065c356da39a9c2a9f7d4237a0223dcee7f978
asosawelford <alejandrososawelford@gmail.com>
2023-05-04 14:07:48 -0300

    view page update

--- public/assets/js/view.js
+++ public/assets/js/view.js
@@ -2,15 +2,15 @@ import { url } from "./config.js";
 
 // Create request object
 const object = {};
-object['id_melody_mode'] = 'jonico';
+object['id_melody_mode'] = 'mixolidio';
 
 const requestJSON = JSON.stringify(object);
-console.log(requestJSON);
+// console.log(requestJSON);
 
 async function query(id_melody_mode) {
     const response = await fetch(
-        //"http://localhost:8000/collective_api/mode_data/",
-        url + "/collective_api/mode_data/",
+        //"http://localhost:8000/profiles_api/mode_data/",
+        url + "/profiles_api/mode_data/",
         {
             headers: new Headers({ 'Content-type': 'application/json' }),
             method: "POST",
@@ -22,28 +22,43 @@ async function query(id_melody_mode) {
     return result;
 }
 
-query(requestJSON).then((response) => {
-    //console.log(JSON.stringify(response));
-    //console.log(response.json());
-    response.json().then(body => console.log(body) || body)
-    .then(body => myTags = body['word_list'] || body)
-    .then(body => num_participants = body['num_participants'] || body)
-    .then(body => last_refresh = body['last_refresh']|| body)
-});
+// query(requestJSON).then((response) => {
+    // console.log(JSON.stringify(response));
+    // console.log(response.json());
+    // response.json().then(body => console.log(body) || body)
+    // .then(body => myTags = body['word_list'] || body)
+    // .then(body => alegria = body['alegria'] || body)
+    // .then(body => sorpresa = body['sorpresa'] || body)
+    // .then(body => miedo = body['miedo'] || body)
+    // .then(body => asco = body['asco'] || body)
+    // .then(body => enojo = body['enojo'] || body)
+    // .then(body => pos = body['pos'] || body)
+    // .then(body => neu = body['neu'] || body)
+    // .then(body => neg = body['neg'] || body)
 
-const myTags = [
-    'alegría', 'esperanza', 'contento', 'paz',
-    'bueno', 'soleado', 'armonioso', 'dulce',
-    'aire', 'aire', 'alegría', 'paz',
-    'aire', 'paz', 'paz', 'yendo',
-    'árboles', 'vida', 'cajas', 'violeta',
-    'castillo', 'monitores', 'papeles', 'impresora',
-    'auriculares', 'billetera', 'magnífico', 'teclas',
-    'piano', 'treinta', 'treinta', 'dieciocho',
-    'televisor', 'computadora', 'escritorio', 'escritorio',
-    'mate', 'silla', 'silla', 'billetera', 'parlante', 'parlante', 'dios', 'guerra',
-    'guerra', 'silla', 'termo'
-];
+    // .then(body => num_participants = body['num_participants'] || body)
+    // .then(body => last_refresh = body['last_refresh']|| body)
+// });
+
+const response = await query(requestJSON);
+const response_json =  await response.json()
+console.log(response_json)
+
+const myTags = response_json['word_lists'] // Todavía no se puede usar, ya que tiene la frecuencia de cada palabra.
+
+// const myTags = [
+//     'alegría', 'esperanza', 'contento', 'paz',
+//     'bueno', 'soleado', 'armonioso', 'dulce',
+//     'aire', 'aire', 'alegría', 'paz',
+//     'aire', 'paz', 'paz', 'yendo',
+//     'árboles', 'vida', 'cajas', 'violeta',
+//     'castillo', 'monitores', 'papeles', 'impresora',
+//     'auriculares', 'billetera', 'magnífico', 'teclas',
+//     'piano', 'treinta', 'treinta', 'dieciocho',
+//     'televisor', 'computadora', 'escritorio', 'escritorio',
+//     'mate', 'silla', 'silla', 'billetera', 'parlante', 'parlante', 'dios', 'guerra',
+//     'guerra', 'silla', 'termo'
+// ];
 
 
 var tagCloud = TagCloud('.content', myTags,{
