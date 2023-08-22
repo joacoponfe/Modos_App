1a8e2466f2e89da98f1af2af601261c9a3187e3f
Joaquín Ponferrada <joacoponfe@gmail.com>
2023-08-22 14:25:35 -0300

    HP Probook config y access.log

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,7 +1,8 @@
-//const url = "http://localhost:8000";
+const url = "http://localhost:8000";
+const urlFront = "http//localhost:3000";
 // const url = "http://127.0.0.1:8000";
-const url = "http://172.28.0.30:8000"; // Ethernet Suipacha
-const urlFront = "http://172.28.0.30:3000"; // Ethernet Suipacha
+//const url = "http://172.28.0.30:8000"; // Ethernet Suipacha
+//const urlFront = "http://172.28.0.30:3000"; // Ethernet Suipacha
 // const url = "http://172.22.0.152:8000";
 //const url = "http://172.22.0.155:8000"; // WiFi Suipacha
 //const url = "http://192.168.245.58:8000"; // Ethernet C3
