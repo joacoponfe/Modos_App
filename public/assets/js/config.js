7560eb5ee7f0923b6a32fa1eb091bc6d799cfeab
joacoponfe <joacoponfe@gmail.com>
2023-08-23 11:33:54 -0300

    Stable version A1085 23/8

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -1,5 +1,5 @@
-const url = "http://localhost:8000";
-const urlFront = "http//localhost:3000";
+//const url = "http://localhost:8000";
+//const urlFront = "http//localhost:3000";
 // const url = "http://127.0.0.1:8000";
 //const url = "http://172.28.0.30:8000"; // Ethernet Suipacha
 //const urlFront = "http://172.28.0.30:3000"; // Ethernet Suipacha
