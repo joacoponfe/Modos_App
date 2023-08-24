9f8e536f9992abb4172fe2c85855686e0294b913
joacoponfe <joacoponfe@gmail.com>
2023-08-24 16:18:56 -0300

    Prepilotaje UDESA

--- public/assets/js/view_mosaic.js
+++ public/assets/js/view_mosaic.js
@@ -6,16 +6,19 @@ function displayImageMosaic() {
         "images/stable_diff/ASW912_ej.jpg",
         "images/stable_diff/BAM490_f2.jpg",
         "images/stable_diff/RAG239_f5.jpg",
+        "images/stable_diff/GDG532_j6.jpg",
+        "images/stable_diff/IS787_e7.jpg",
+        "images/stable_diff/LPC332_li1.jpg",
+        "images/stable_diff/DES111_j7.jpg"
     ];
-    var initials = ['AMG', 'ASW', 'BAM','RAG'];
-
+    var initials = ['AMG', 'ASW', 'BAM','RAG', 'GDG', 'IS', 'LPC', 'DES'];
 
     // Container element for the mosaic
-    var mosaicContainer = document.getElementById("imageMosaic");
+    var mosaicGrid = document.getElementById("imageMosaic");
 
-    // Create the mosaic container
-    var mosaicGrid = document.createElement("div");
-    mosaicGrid.className = "mosaic-grid";
+    // // Create the mosaic container
+    // var mosaicGrid = document.createElement("div");
+    // mosaicGrid.className = "mosaic-grid";
 
     // Loop through the image URLs and create mosaic tiles
     for (var i = 0; i < imageUrls.length; i++) {
@@ -27,6 +30,7 @@ function displayImageMosaic() {
         var image = document.createElement("img");
         image.src = imageUrls[i];
         image.alt = "Image " + (i + 1);
+        image.style.borderRadius = "10px";
 
         // Create the text element
         var text = document.createElement("p");
@@ -41,7 +45,7 @@ function displayImageMosaic() {
     }
 
     // Append the mosaic grid to the mosaic container
-    mosaicContainer.appendChild(mosaicGrid);
+    //mosaicContainer.appendChild(mosaicGrid);
 }
 
 // Call the function to display the image mosaic
