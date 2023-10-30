ccde379a0df1f8ac965082778c5dc6711eb3cb96
joacoponfe <joacoponfe@gmail.com>
2023-10-30 17:39:09 -0300

    Avances en trivia, ajustes de diseño en totems

---
+++ public/assets/js/trivia_intro.js
@@ -0,0 +1,14 @@
+const countdown = document.getElementById("countdown");
+
+let timeLeft = 4;
+let timerId = setInterval(redirect, 1000);
+
+function redirect() {
+    if (timeLeft == 0) {
+        clearTimeout(timerId);
+        window.location.href = "trivia.html";
+    } else {
+        countdown.innerHTML = 'Las preguntas comienzan en ' + timeLeft + ' segundos...';
+        timeLeft--;
+    }
+}
