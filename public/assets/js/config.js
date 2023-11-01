f009e9ba6b1b5999f305c18af4b7c9c177b93c83
Joaquín Ponferrada <joacoponfe@gmail.com>
2023-11-01 14:06:06 -0300

    Ajustes para pantalla gigante de mosaico

--- public/assets/js/config.js
+++ public/assets/js/config.js
@@ -9,13 +9,18 @@
 //const url = "http://172.28.0.30:8000"; // Ethernet Suipacha
 //const urlFront = "http://172.28.0.30:3000"; // Ethernet Suipacha
 // const url = "http://172.22.0.152:8000";
-const url = "http://172.22.0.29:8000"; // WiFi Suipacha
-const urlFront = "http://172.22.0.29:3000"; // WiFi Suipacha
+// const url = "http://172.22.0.234:8000"; // WiFi Suipacha
+// const urlFront = "http://172.22.0.234:3000"; // WiFi Suipacha
 //const url = "http://172.22.1.59:8000"; // Wifi Udesa
 //const urlFront = "http://172.22.1.59:3000" // Wifi Udesa
 //const url = "http://168.195.52.130:8000"; // Wifi Udesa
 //const urlFront = "http://168.195.52.130:3000" // Wifi Udesa
+//const url = "http://192.168.182.227:8000"; // WiFi C3 Red Invitados
+//const urlFront = "http://192.168.182.227:3000"; // WiFi C3 Red Invitados
 //const url = "http://192.168.245.58:8000"; // Ethernet C3
+const url = "http://192.168.124.98:8000";
+const urlFront = "http://192.168.124.98:3000";
+
 const language = "es";
 const songs_version = "off";
 export { url, urlFront, language, songs_version };
